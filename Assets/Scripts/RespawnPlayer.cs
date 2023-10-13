35ccfcad315d9be7ec7ae8b217eaecac6fe4f008
EIdras <simon72610@gmail.com>
2023-10-13 17:00:59 +0200

    Respawn & Snow / Rain fx

--- Assets/Scripts/RespawnPlayer.cs
+++ Assets/Scripts/RespawnPlayer.cs
@@ -1,14 +1,21 @@
+using Cinemachine;
 using UnityEngine;
 
 public class RespawnPlayer : MonoBehaviour
 {
-    [SerializeField] private Transform playerSpawnPoint;
-    private void OnTriggerEnter2D(Collider2D other)
+    [SerializeField] public Transform playerSpawnPoint;
+
+    [Header("Camera")]
+    [SerializeField] public Transform spawnAreaCamera;
+    public CinemachineVirtualCamera virtualCamera;
+    private void OnTriggerExit2D(Collider2D other)
     {
-        if(other.CompareTag("Player")) // Assurez-vous que votre joueur a le tag "Player"
+        if(other.CompareTag("Player"))
         {
             // Téléporte le joueur à son point de spawn
+            Debug.Log("Player has fallen");
             other.transform.position = playerSpawnPoint.position;
+            virtualCamera.Follow = spawnAreaCamera;
         }
     }
 }
